4638355977aa8cee1494687ef4cb6f7a477cdddf
forkmeharderdaddy <94821609+forkmeharderdaddy@users.noreply.github.com>
2024-03-09 11:28:35 -1000

    added widgets

---
+++ components/widgets/radingviewwidget.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef, memo } from 'react';
+
+function TradingViewWidget() {
+  const container = useRef();
+
+  useEffect(
+    () => {
+      const script = document.createElement("script");
+      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = `
+        {
+          "autosize": true,
+          "symbol": "UNISWAP:PEPLWETH_1BFD93.USD",
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "dark",
+          "style": "1",
+          "locale": "en",
+          "enable_publishing": false,
+          "allow_symbol_change": true,
+          "support_host": "https://www.tradingview.com"
+        }`;
+      container.current.appendChild(script);
+    },
+    []
+  );
+
+  return (
+    <div className="tradingview-widget-container" ref={container} style={{ height: "200%", width: "100%" }}></div>
+    
+  );
+}
+
+export default memo(TradingViewWidget);
