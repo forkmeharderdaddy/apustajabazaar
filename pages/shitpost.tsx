5ad39c1b20c8c4aa30d507b4c34ed90de44de5ed
BitWonka <79578887+BitWonka@users.noreply.github.com>
2024-06-03 06:50:39 +0200

    feat: cutom mint

--- pages/shitpost.tsx
+++ pages/shitpost.tsx
@@ -1,38 +1,32 @@
 import type { NextPage } from "next";
-import { MediaRenderer, useAddress } from "@thirdweb-dev/react"
 import styles from "../styles/Home.module.css";
 import { useRef, useState } from "react";
 import axios from "axios";
 import {
-  useLazyMint,
-  useContract,
-  Web3Button,
-} from "@thirdweb-dev/react";
-import { MARKETPLACE_ADDRESS } from "../const/contractAddresses";
+  TESTNET_CONTRACT_ADDRESS,
+  twClient,
+  twContract,
+} from "../const/contractAddresses";
 import toast from "react-hot-toast";
-import ShitPostERC20 from "./shitpost_erc20";
-import { nftABI } from "./nft"
-import Web3 from 'web3';
-
-const JWT = process.env.NEXT_PUBLIC_PINATA_JWT || "";
-
-const web3 = new Web3(process.env.NEXT_PUBLIC_WEB3_URL) || '';
-
-const private_key = process.env.NEXT_PUBLIC_PRIVATE_KEY || "";
+import {
+  generateMintSignature,
+  mintWithSignature,
+} from "thirdweb/extensions/erc721";
+import { MediaRenderer } from "thirdweb/react";
+import { useAddress, useSigner, Web3Button } from "@thirdweb-dev/react";
+import { ethers5Adapter } from "thirdweb/adapters/ethers5";
+import { sendTransaction } from "thirdweb";
 
 const Home: NextPage = () => {
-  const { contract } = useContract(MARKETPLACE_ADDRESS);
-  const {
-    mutateAsync: lazyMint,
-    isLoading,
-    error,
-  } = useLazyMint(contract);
   const address = useAddress();
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [imageURL, setImageUrl] = useState<string | null>(null);
   const [shitpostName, setShitpostName] = useState<string>("");
   const [shitpostDescription, setShitpostDescription] = useState<string>("");
   const [file, setFile] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const signer = useSigner();
 
   const processFile = (file: File) => {
     const reader = new FileReader();
@@ -59,84 +53,65 @@ const Home: NextPage = () => {
     setFile(null);
   };
 
-  const mintNFT = async () => {
+  const mintNftNew = async () => {
+    setIsLoading(true);
     try {
-      if (!contract) {
-        return;
+      if (!signer) {
+        toast.error("Please connect your wallet");
+        throw new Error("Please connect your wallet");
       }
 
-      if (file === null) {
+      /* Workaround to use V5 */
+      const account = await ethers5Adapter.signer.fromEthers({ signer });
+      if (!account) {
+        toast.error("Please connect your wallet");
+        throw new Error("Please connect your wallet");
+      }
+
+      if (!file || !file.type.startsWith("image/")) {
         toast.error("Invalid Image");
-        return;
+        throw new Error("Invalid Image");
       }
-
-      const MINTER_ROLE = web3.utils.keccak256("MINTER_ROLE");
-      const web3contract = new web3.eth.Contract(nftABI, MARKETPLACE_ADDRESS);
-      const ownerAccount = web3.eth.accounts.privateKeyToAccount(private_key);
-      const functionData = web3contract.methods.grantRole(MINTER_ROLE, address).encodeABI();
-      const nonce = await web3.eth.getTransactionCount(ownerAccount.address);
-      const gasPrice = await web3.eth.getGasPrice();
-      const gasLimit = 3000000; // You may need to adjust this value
-
-      const txObject = {
-        from: ownerAccount.address,
-        to: MARKETPLACE_ADDRESS,
-        data: functionData,
-        nonce: web3.utils.toHex(nonce),
-        gasPrice: web3.utils.toHex(gasPrice),
-        gasLimit: web3.utils.toHex(gasLimit),
-      };
-
-      // Sign the transaction
-      const signedTx = await web3.eth.accounts.signTransaction(txObject, private_key);
-
-      // Send the transaction
-      const txReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-      console.log('Transaction receipt:', txReceipt);
 
       const formData = new FormData();
-      formData.append('file', file as File);
+      formData.append("image", file);
+      formData.append("address", account.address);
+      formData.append("name", shitpostName);
+      formData.append("description", shitpostDescription);
 
-      const pinataOptions = JSON.stringify({
-        cidVersion: 0,
-      })
-      formData.append('pinataOptions', pinataOptions);
-      const { data: ipfs_data } = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+      const { data } = await axios.post<
+        Awaited<ReturnType<typeof generateMintSignature>>
+      >("/api/getTransaction", formData, {
         headers: {
-          'Content-Type': `multipart/form-data;`,
-          'Authorization': `Bearer ${JWT}`
-        }
+          "Content-Type": "multipart/form-data",
+        },
       });
 
-      await lazyMint({
-        metadatas: [
-          {
-            name: shitpostName,
-            description: shitpostDescription,
-            image: `ipfs://${ipfs_data.IpfsHash}`,
-          },
-        ],
-      })
+      const transaction = mintWithSignature({
+        contract: twContract,
+        payload: data.payload,
+        signature: data.signature,
+      });
 
-      toast.success("Success!")
-
-    } catch (err) {
-      console.log("mint error:", err);
-      toast.error("Something went wrong!")
+      await sendTransaction({ transaction, account });
+      toast.success(`Shitpost created`);
+    } catch (error: unknown) {
+      console.error(error);
+      toast.error(`An error occured`);
+    } finally {
+      setIsLoading(false);
     }
-  }
+  };
 
   return (
-    <div className={styles.container} style={{ marginTop: '100px' }}>
+    <div className={styles.container} style={{ marginTop: "100px" }}>
       {address ? (
         <div className={styles.mintContainer}>
           <div className={styles.mintContainerSection}>
             <h1 style={{ marginTop: "60px" }}>ShitPost Image</h1>
-            <div
-              className={styles.fileContainer}
-              onClick={hadleFileSelect}
-            >
-              <input type="file"
+            <div className={styles.fileContainer} onClick={hadleFileSelect}>
+              <input
+                type="file"
                 accept="image/*"
                 ref={fileInputRef}
                 style={{ display: "none" }}
@@ -158,16 +133,24 @@ const Home: NextPage = () => {
                   <p>Click to upload image</p>
                 </div>
               ) : (
-                <div style={{ width: "300px", height: "300px", display: "flex", flexDirection: "column", gap: 2 }}>
+                <div
+                  style={{
+                    width: "300px",
+                    height: "300px",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 2,
+                  }}
+                >
                   <MediaRenderer
+                    client={twClient}
                     src={imageURL}
                     height="100%"
                     width="100%"
                   />
-                  <button
-                    onClick={reset}
-                    className={styles.resetButton}
-                  >Reset</button>
+                  <button onClick={reset} className={styles.resetButton}>
+                    Reset
+                  </button>
                 </div>
               )}
             </div>
@@ -191,19 +174,18 @@ const Home: NextPage = () => {
               className={styles.metadataInput}
             />
           </div>
-          <div style={{ marginTop: '30px', display: "flex", gap: "5px" }}>
+          <div style={{ marginTop: "30px", display: "flex", gap: "5px" }}>
             <Web3Button
-              contractAddress={MARKETPLACE_ADDRESS}
+              contractAddress={TESTNET_CONTRACT_ADDRESS}
               className={styles.mintButton}
-              action={() => mintNFT()}
+              action={() => mintNftNew()}
             >
               {isLoading ? "Minting..." : "Mint ShitPost"}
             </Web3Button>
-            <ShitPostERC20 file={file as File} shitpostName={shitpostName} shitpostDescription={shitpostDescription} />
           </div>
         </div>
       ) : (
-        <div style={{ marginTop: '100px' }}>
+        <div style={{ marginTop: "100px" }}>
           <h1>Sign in to mint an NFT</h1>
         </div>
       )}
